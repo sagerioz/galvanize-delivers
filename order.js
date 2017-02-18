fb241cd61c1eb9ab3c68ab32930e23f20b8ffcb7
sagerioz <ericaepperson27@yahoo.com>
2017-02-17 17:40:59 -0700

    jquery order working

--- order.js
+++ order.js
@@ -1,57 +1,97 @@
 $(document).ready(function() {
-  var orderObject = {
-    "burger": {
-      "name": "Burger",
-      "price": 8.99,
-      "quantity": 0
+  let orderObject = {
+    burger: {
+      name: "Burger",
+      price: 8.99,
+      quantity: 0
     },
-    "ribs": {
-      "name": "Ribs",
-      "price": 14.99,
-      "quantity": 0
+    ribs: {
+      name: "Ribs",
+      price: 14.99,
+      quantity: 0
     },
-    "pie": {
-      "name": "Pie",
-      "price": 11.99,
-      "quantity": 0
+    pie: {
+      name: "Pie",
+      price: 11.99,
+      quantity: 0
     },
-    "icecream": {
-      "name": "Icecream",
-      "price": 7.99,
-      "quantity": 0
+    icecream: {
+      name: "Icecream",
+      price: 7.99,
+      quantity: 0
     }
   }
 
+  let total = 0;
+  let name = orderObject.burger.name
+  let price = orderObject.burger.price
+  //console.log("Quantity", itemQuantity);
+  //total = orderObject.burger.price * itemQuantity;
 
-  // Add Item to Console Log
+  // console.log("total...", total);
 
-  $("#burger").click(function(e) {
-    e.preventDefault();
-    let total = 0;
-    let name = orderObject['burger']['name']
-    let itemQuantity = orderObject['burger']['quantity'] += 1
-    let price = orderObject['burger']['price']
-    console.log("Quantity", itemQuantity);
-    total = orderObject['burger']['price'] * itemQuantity;
-    let tax = (Math.round(total * .08)).toFixed(2)
-    console.log("total...", total);
+  // Add Item to Order list
+  // try to target the whole container
+  $(".container").click(function(event) {
+    event.preventDefault();
+    //console.log("EVENT TARGET", event.target);
+    if ($(event.target).is('.button')) {
+      // console.log('herererere');
+      let currID = $(event.target).attr('id')
+      // console.log("EVENT TARGET", currID);
+      orderObject[currID].quantity += 1
+      console.log(orderObject[currID].quantity);
+      createTable(orderObject);
+      // console.log("EVENT TARGET", $(event.target));
 
-    // Add Item to OrderList
-    $("#orderTable").prepend("<tr><td>" + "<img src='img/delete.png' id='delete' height='15px' width='15px'" + "</td><td>" + name + "</td><td>$" + price + "</tr>");
-    if($("#orderTable>tr").hasClass("placeholder")){
-      $("#orderTable")[0].removeChild($('#orderTable')[0].lastChild);
-      $("#orderTable")[0].removeChild($('#orderTable')[0].lastChild);
     }
-    // $("#orderTable")[0].removeChild($('#orderTable')[0].lastChild);
-    //     $("#orderTable")[0].removeChild($('#orderTable')[0].lastChild);
-    let temp = $("#orderTable").last()
-    console.log("last", temp);
 
-    $("#sub").text('$ ' + total)
-    $('#tax').text('$ ' + tax)
-    $('#grandTotal').text('$ ' + (total + tax))
 
   })
+
+
+
+  //--------------build table-------------->
+
+  function createTable(data) {
+
+    let table = $('table')
+    let tbody = $("#orderTable")
+    tbody.html('')
+    //console.log('MY TABLE=',table.children());
+
+    let subtotal = 0;
+    for (name in orderObject) {
+      if (orderObject[name].quantity !== 0) {
+
+        // console.log("QUANTITY", orderObject[name].quantity);
+        // console.log("price", orderObject[name].price);
+        let currentTotal = (orderObject[name].quantity * orderObject[name].price)
+
+
+        subtotal += currentTotal
+
+        let tax = (subtotal * 0.08).toFixed(2)
+        let grandTotal = parseInt(subtotal) + parseInt(tax)
+        let row = $('<tr></tr>').appendTo(tbody)
+        console.log("ROW", row);
+        $('#sub').text("$" + subtotal)
+        $('#tax').text("$" + tax)
+        $('#grandTotal').text("$" + grandTotal)
+        //img
+        $("<td><img src='img/delete.png' id='delete' height='15px' width='15px'></td>").appendTo(row)
+        //menu item
+        $(`<td>${name}</td>`).appendTo(row)
+        //price
+        $(`<td>${currentTotal}</td>`).appendTo(row)
+
+      }
+    }
+
+    // console.log("TABLE:", table);
+
+    return table[0];
+  }
 
 
 
